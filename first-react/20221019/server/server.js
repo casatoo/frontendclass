3c9f279b4a782b2eca859ed70dc80172d4757875
sanguk <tkddnr6079@naver.com>
2022-10-24 21:49:57 +0900

    2022.10.24

--- first-react/20221019/server/server.js
+++ first-react/20221019/server/server.js
@@ -19,13 +19,15 @@ const port = 4000;
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.get("/test", (req, res) => {
-  DB.test.push("테스트중입니다.");
-  console.log(DB.test);
-  res.send({
-    code: "sucess",
-    msg: "테스트 성공",
-  });
+
+app.get("/add/cart", (req, res) => {
+  DB.장바구니.push(req.query);
+  res.send("아이템 넣기 성공");
+});
+app.get("/delete/cart", (req, res) => {
+  console.log(req.query);
+  DB.장바구니 = DB.장바구니.filter((item) => item.name !== req.query.name);
+  res.send(DB.장바구니);
 });
 
 app.get("/myCart", (req, res) => {
--- first-react/20221019/src/App.js
+++ first-react/20221019/src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+function 상품정보(props) {
+  const { item, 모달창열기, 상품액션, 버튼 } = props;
+
+  return (
+    <div>
+      <div className="item">
+        <div className="item-block">
+          <div className="image-area">
+            <img
+              onClick={모달창열기.bind(this, item)}
+              src={item.image}
+              alt="상품이미지"
+              className="image"
+            />
+          </div>
+          <div className="name">{item.name}</div>
+          <div className="description">{item.descrition}</div>
+          <div className="bottom-area">
+            <div className="price">{item.price}</div>
+            <div className="button" onClick={상품액션.bind(this, item)}>
+              <버튼 />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   const [products, setProducts] = React.useState([
@@ -41,11 +71,6 @@ function App() {
     });
   }, []);
 
-  const 장바구니삭제 = (인덱스번호) => {
-    const cloneMyCart = [...myCart];
-    cloneMyCart.splice(인덱스번호, 1);
-    setMyCart(cloneMyCart);
-  };
   const 총금액 = React.useMemo(() => {
     let 금액 = 0;
     myCart.forEach((item, index) => {
@@ -54,21 +79,14 @@ function App() {
     return 금액;
   }, [myCart]);
 
-  const 장바구니넣기 = async (item) => {
-    await axios({
-      url: "http://localhost:4000/test",
-      params: item,
-    })
-      .then((Response) => {})
-      .catch((e) => {
-        console.log("네트워크 요청 에러", e);
-      });
-  };
-  const 서버에있는장바구니가져오기 = async () => {
-    await axios({
+  const 서버에있는장바구니가져오기 = () => {
+    axios({
       url: "http://localhost:4000/myCart",
     })
-      .then((Response) => {})
+      .then((Response) => {
+        const cloneMyCart = [...Response.data];
+        setMyCart(cloneMyCart);
+      })
       .catch((e) => {
         console.log("네트워크 요청 에러", e);
       });
@@ -77,6 +95,56 @@ function App() {
   React.useEffect(() => {
     서버에있는장바구니가져오기();
   }, []);
+
+  const 모달창열기 = (item) => {
+    const cloneShowModal = { ...showModal };
+    cloneShowModal.show = true;
+    cloneShowModal.image = item.image;
+    setShowModal(cloneShowModal);
+  };
+
+  const 상품장바구니담기 = async (item) => {
+    const cloneMyCart = [...myCart];
+
+    const 이미갖고있는상품 = cloneMyCart.find((myItem) => {
+      return myItem.name === item.name;
+    });
+
+    if (이미갖고있는상품) {
+      alert("이미 갖고 있습니다.");
+      return;
+    }
+
+    await axios({
+      method: "get",
+      url: "http://127.0.0.1:4000/add/cart",
+      dataType: "json",
+      params: item,
+    })
+      .then((res) => {})
+      .catch((e) => {
+        console.log(e);
+      });
+
+    cloneMyCart.push(item);
+    setMyCart(cloneMyCart);
+  };
+
+  const 상품장바구니비우기 = async (item) => {
+    await axios({
+      url: "http://localhost:4000/delete/cart",
+      method: "get",
+      dataType: "json",
+      params: item,
+    })
+      .then((res) => {
+        const cloneMyCart = [...res.data];
+        setMyCart(cloneMyCart);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   return (
     <div className="App">
@@ -92,55 +160,15 @@ function App() {
             {products &&
               products.map((item, index) => {
                 return (
-                  <div key={`products-${index}`}>
-                    <div className="item">
-                      <div className="item-block">
-                        <div className="image-area">
-                          <img
-                            onClick={() => {
-                              const cloneShowModal = { ...showModal };
-                              cloneShowModal.show = true;
-                              cloneShowModal.image = item.image;
-                              setShowModal(cloneShowModal);
-                            }}
-                            src={item.image}
-                            alt="상품이미지"
-                            className="image"
-                          />
-                        </div>
-                        <div className="name">{item.name}</div>
-                        <div className="description">{item.descrition}</div>
-                        <div className="bottom-area">
-                          <div className="price">{item.price}</div>
-                          <div
-                            className="button"
-                            onClick={() => {
-                              // myCart 배열에 똑같은 상품이 있는지 확인
-                              // 똑같은 상품있으면 push X
-                              장바구니넣기(item);
-                              const cloneMyCart = [...myCart];
-
-                              const 이미갖고있는상품 = cloneMyCart.find(
-                                (myItem) => {
-                                  return myItem.name === item.name;
-                                }
-                              );
-
-                              if (이미갖고있는상품) {
-                                alert("이미 갖고 있습니다.");
-                                return;
-                              }
-
-                              cloneMyCart.push(item);
-                              setMyCart(cloneMyCart);
-                            }}
-                          >
-                            <p>ADD TO CART</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <상품정보
+                    key={`product-${index}`}
+                    item={item}
+                    모달창열기={모달창열기}
+                    상품액션={상품장바구니담기}
+                    버튼={() => {
+                      return <p>ADD TO CART</p>;
+                    }}
+                  />
                 );
               })}
           </div>
@@ -157,36 +185,15 @@ function App() {
             {myCart ? (
               myCart.map((item, index) => {
                 return (
-                  <div key={`products-${index}`}>
-                    <div className="item">
-                      <div className="item-block">
-                        <div className="image-area">
-                          <img
-                            onClick={() => {
-                              const cloneShowModal = { ...showModal };
-                              cloneShowModal.image = item.image;
-                              cloneShowModal.show = true;
-                              setShowModal(cloneShowModal);
-                            }}
-                            src={item.image}
-                            alt="상품이미지"
-                            className="image"
-                          />
-                        </div>
-                        <div className="name">{item.name}</div>
-                        <div className="description">{item.descrition}</div>
-                        <div className="bottom-area">
-                          <div className="price">{item.price}</div>
-                          <div
-                            className="button"
-                            onClick={장바구니삭제.bind(this, index)}
-                          >
-                            <p>REMOVE</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <상품정보
+                    key={`myCart-${index}`}
+                    모달창열기={모달창열기}
+                    item={item}
+                    상품액션={상품장바구니비우기}
+                    버튼={() => {
+                      return <p style={{ color: "red" }}>DELETE</p>;
+                    }}
+                  />
                 );
               })
             ) : (
@@ -220,4 +227,48 @@ function App() {
   );
 }
 
-export default App;
+function Main() {
+  const { loginUser } = React.useContext(StoreContext);
+  console.log(loginUser);
+  return <div>메인페이지</div>;
+}
+
+function Sub() {
+  const navigation = useNavigate();
+  return (
+    <div>
+      서브페이지{" "}
+      <div
+        onClick={() => {
+          navigation("/");
+        }}
+      >
+        메인페이지로 이동
+      </div>
+    </div>
+  );
+}
+
+const StoreContext = React.createContext({});
+
+function Test() {
+  const [loginUser, setLoginUser] = React.useState({
+    id: "asd123",
+    name: "성민",
+    age: 12,
+  });
+  return (
+    <StoreContext.Provider
+      value={{
+        loginUser,
+      }}
+    >
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/sub" element={<Sub />} />
+      </Routes>
+    </StoreContext.Provider>
+  );
+}
+
+export default Test;
